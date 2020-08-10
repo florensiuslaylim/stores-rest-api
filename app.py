45e512c278ef11c61de3a0cd82042a5cdebbb457
Florensius <florensiusxodc@gmail.com>
2020-08-10 12:05:26 +0700

    Remove fault

--- app.py
+++ app.py
@@ -7,16 +7,11 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
  
-app = Flask(__name__) venv/bin/activate
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'floren'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity )
 
 api.add_resource(Store, '/store/<string:name>')
